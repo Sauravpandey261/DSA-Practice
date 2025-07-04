4356aea98a5da9bc524bc3d65bbb9bdefd9a561a
Sauravpandey261 <sauravpandey261@gmail.com>
2025-07-04 13:49:27 +0530

    Added removeDuplicate

--- removeDuplicates.cpp
+++ removeDuplicates.cpp
@@ -1,8 +1,26 @@
 #include<iostream>
 using namespace std;
 
+void removeDuplicates(string str,string ans,int i, int map[26]){
+    if(i==str.size()){
+        cout<<"ans: "<<ans<<endl;
+    }
+    char ch=str[i];
+    int mapIdx=(int)(ch-'a');
+    if(map[mapIdx]){
+        removeDuplicates(str,ans,i++,map);
+    }
+    else{
+        map[mapIdx]=true;
+        removeDuplicates(str,ans+str[i],i++,map);
+    }
+}
+
 int main()
 {
-    
+    string str="appnnacollege";
+    string ans="";
+    int map[26]={false};
+    removeDuplicates(str,ans,0,map);
     return 0;
 }
