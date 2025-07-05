f3ea4ee4136c5f54df0c4ad97819b377275d6b2f
Sauravpandey261 <sauravpandey261@gmail.com>
2025-07-05 16:12:17 +0530

    Added mergeSort

--- removeDuplicates.cpp
+++ removeDuplicates.cpp
@@ -4,15 +4,16 @@ using namespace std;
 void removeDuplicates(string str,string ans,int i, int map[26]){
     if(i==str.size()){
         cout<<"ans: "<<ans<<endl;
+        return;
     }
     char ch=str[i];
     int mapIdx=(int)(ch-'a');
     if(map[mapIdx]){
-        removeDuplicates(str,ans,i++,map);
+        removeDuplicates(str,ans,i+1,map);
     }
     else{
         map[mapIdx]=true;
-        removeDuplicates(str,ans+str[i],i++,map);
+        removeDuplicates(str,ans+str[i],i+1,map);
     }
 }
 
